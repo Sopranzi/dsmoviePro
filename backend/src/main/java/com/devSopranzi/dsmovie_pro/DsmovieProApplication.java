6c17e5781347af905eed311356db2839376edb55
Mateus Neves Sopranzi <mateussopranzi@gmail.com>
2022-03-08 16:28:20 -0300

    Project created

---
+++ backend/src/main/java/com/devSopranzi/dsmovie_pro/DsmovieProApplication.java
@@ -0,0 +1,13 @@
+package com.devSopranzi.dsmovie_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieProApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devSopranzi/dsmovie_pro/DsmovieProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSopranzi.dsmovie_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
